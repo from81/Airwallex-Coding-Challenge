3ffde8ce019b54dc29ef3a0a4d2e059b2d1338b3
Kai <kaiyhirota@gmail.com>
2021-07-30 23:05:54 +1000

    clean up

--- src/main/java/com/airwallex/codechallenge/monitor/Monitor.java
+++ src/main/java/com/airwallex/codechallenge/monitor/Monitor.java
@@ -7,8 +7,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Optional;
